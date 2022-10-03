190129982d3728250b3c066b97440beb4665dd7d
rusber364 <rusber364@gmail.com>
2022-10-04 00:04:59 +0300

    add customize Button and Nav

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,21 +1,24 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import type { RoutesParamList } from '../types/RoutesParamList'
-import { Button } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+import { Button } from './common/Button'
 
 type Props = NativeStackScreenProps<RoutesParamList>
 
 export function Navigation({ navigation }: Props) {
-  const navigateToHome = () => navigation.navigate('Home')
   const navigateToTeam = () => navigation.navigate('Team')
   const navigateToGameMode = () => navigation.navigate('GameMode')
   const navigateToAchievement = () => navigation.navigate('Achievement')
 
   return (
-    <>
-      <Button title="Home" onPress={navigateToHome} />
-      <Button title="Team" onPress={navigateToTeam} />
-      <Button title="GameMode" onPress={navigateToGameMode} />
-      <Button title="Achievement" onPress={navigateToAchievement} />
-    </>
+    <View style={style.nav}>
+      <Button onPress={navigateToGameMode}>Game</Button>
+      <Button onPress={navigateToTeam}>Team</Button>
+      <Button onPress={navigateToAchievement}>Achievement</Button>
+    </View>
   )
 }
+
+const style = StyleSheet.create({
+  nav: { alignSelf: 'stretch', paddingHorizontal: 15 },
+})
