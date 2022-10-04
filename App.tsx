d1ee5485d1474121e677b44fb39af4da6fcd2120
rusber364 <rusber364@gmail.com>
2022-10-04 18:50:37 +0300

    move screens to comp Navigator

--- App.tsx
+++ App.tsx
@@ -1,20 +1,6 @@
 import { NavigationContainer, useNavigationContainerRef } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { useFlipper } from '@react-navigation/devtools'
-
-import type { RoutesList } from './app/types/RouteList'
-import { HomeScreen } from './app/screens/HomeScreen'
-import { GameModeScreen } from './app/screens/GameModeScreen'
-import { TeamScreen } from './app/screens/TeamScreen'
-import { AchievementScreen } from './app/screens/AchievementScreen'
-import { LeagueScreen } from './app/screens/LeagueScreen'
-import { OptionModal } from './app/components/OptionModal'
-import { Profile } from './app/components/Profile'
-import { StoreModeScreen } from './app/screens/ScreenInGameMode/StoreModeScreen'
-import { SurvivalModeScreen } from './app/screens/ScreenInGameMode/SurvivalModeScreen'
-import { KnowledgeScreen } from './app/screens/ScreenInGameMode/KnowledgeScreen'
-
-export const Stack = createNativeStackNavigator<RoutesList>()
+import { Navigator } from './app/Navigator'
 
 export default function App() {
   const navigationRef = useNavigationContainerRef()
@@ -23,16 +9,7 @@ export default function App() {
 
   return (
     <NavigationContainer ref={navigationRef}>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="StoreMode" component={StoreModeScreen} />
-        <Stack.Screen name="SurvivalMode" component={SurvivalModeScreen} />
-        <Stack.Screen name="Knowledge" component={KnowledgeScreen} />
-        <Stack.Screen name="GameMode" component={GameModeScreen} />
-        <Stack.Screen name="Team" component={TeamScreen} />
-        <Stack.Screen name="Achievement" component={AchievementScreen} />
-        <Stack.Screen name="League" component={LeagueScreen} />
-      </Stack.Navigator>
+      <Navigator />
     </NavigationContainer>
   )
 }
---
+++ app/Navigator.tsx
@@ -0,0 +1,31 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import type { RoutesList } from './types/RouteList'
+import { HomeScreen } from './screens/HomeScreen'
+import { GameModeScreen } from './screens/GameModeScreen'
+import { TeamScreen } from './screens/TeamScreen'
+import { AchievementScreen } from './screens/AchievementScreen'
+import { LeagueScreen } from './screens/LeagueScreen'
+import { StoreModeScreen } from './screens/ScreenInGameMode/StoreModeScreen'
+import { SurvivalModeScreen } from './screens/ScreenInGameMode/SurvivalModeScreen'
+import { KnowledgeScreen } from './screens/ScreenInGameMode/KnowledgeScreen'
+
+import { Profile } from './components/Profile'
+import { OptionModal } from './components/OptionModal'
+
+const Stack = createNativeStackNavigator<RoutesList>()
+
+export function Navigator() {
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="StoreMode" component={StoreModeScreen} />
+      <Stack.Screen name="SurvivalMode" component={SurvivalModeScreen} />
+      <Stack.Screen name="Knowledge" component={KnowledgeScreen} />
+      <Stack.Screen name="GameMode" component={GameModeScreen} />
+      <Stack.Screen name="Team" component={TeamScreen} />
+      <Stack.Screen name="Achievement" component={AchievementScreen} />
+      <Stack.Screen name="League" component={LeagueScreen} />
+    </Stack.Navigator>
+  )
+}
