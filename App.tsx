fa6210ce40bcc9a55794a2e4fc0e5363ba1b8833
IlliaAdamchuk <Illya.Adamchuk@gamil.com>
2022-10-04 14:28:02 +0200

    add Navigation for game mode

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { NavigationContainer, useNavigationContainerRef } from '@react-navigatio
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { useFlipper } from '@react-navigation/devtools'
 
-import type { RoutesParamList } from './app/types/RoutesParamList'
+import type { RoutesList } from './app/types/RouteList'
 import { HomeScreen } from './app/screens/HomeScreen'
 import { GameModeScreen } from './app/screens/GameModeScreen'
 import { TeamScreen } from './app/screens/TeamScreen'
@@ -10,8 +10,11 @@ import { AchievementScreen } from './app/screens/AchievementScreen'
 import { LeagueScreen } from './app/screens/LeagueScreen'
 import { OptionModal } from './app/components/OptionModal'
 import { Profile } from './app/components/Profile'
+import { StoreModeScreen } from './app/screens/ScreenInGameMode/StoreModeScreen'
+import { SurvivalModeScreen } from './app/screens/ScreenInGameMode/SurvivalModeScreen'
+import { KnowledgeScreen } from './app/screens/ScreenInGameMode/KnowledgeScreen'
 
-const Stack = createNativeStackNavigator<RoutesParamList>()
+export const Stack = createNativeStackNavigator<RoutesList>()
 
 export default function App() {
   const navigationRef = useNavigationContainerRef()
@@ -22,6 +25,9 @@ export default function App() {
     <NavigationContainer ref={navigationRef}>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="StoreMode" component={StoreModeScreen} />
+        <Stack.Screen name="SurvivalMode" component={SurvivalModeScreen} />
+        <Stack.Screen name="Knowledge" component={KnowledgeScreen} />
         <Stack.Screen name="GameMode" component={GameModeScreen} />
         <Stack.Screen name="Team" component={TeamScreen} />
         <Stack.Screen name="Achievement" component={AchievementScreen} />
---
+++ app/types/RouteList.ts
@@ -0,0 +1,4 @@
+import { RoutesParamList } from './RoutesParamList'
+import { RoutesParamListGameMode } from './RoutesParamListGameMode'
+
+export type RoutesList = RoutesParamList & RoutesParamListGameMode
