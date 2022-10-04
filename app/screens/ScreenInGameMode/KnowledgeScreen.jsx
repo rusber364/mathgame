620e53ce0f54c9ecff7e626e7a73b2ec79059498
IlliaAdamchuk <Illya.Adamchuk@gamil.com>
2022-10-04 14:28:02 +0200

    add Navigation for game mode

---
+++ app/screens/ScreenInGameMode/KnowledgeScreen.jsx
@@ -0,0 +1,10 @@
+import { Text } from 'react-native'
+import { Paper } from '../../layout/Paper'
+
+export function KnowledgeScreen() {
+  return (
+    <Paper>
+      <Text>KnowledgeScreen</Text>
+    </Paper>
+  )
+}
