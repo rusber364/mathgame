d0a042e6e60ef52d3a4a740e59ce6b50cbab6625
rusber364 <rusber364@gmail.com>
2022-10-10 01:43:10 +0300

    add changeLanguage langs

--- app/translation.ts
+++ app/translation.ts
@@ -1,16 +1,21 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
 
 import ru from './langs/ru.json'
 import en from './langs/en.json'
 
-export const resources = { en, ru } as const
+export const resources = { en, ru }
 
-i18n.use(initReactI18next).init({
-  resources,
-  lng: 'en',
-  fallbackLng: 'en',
-  interpolation: {
-    escapeValue: false,
-  },
-})
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    resources,
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+  })
+
+export default i18n
