f9ddccc88c855d6d98372887263c926ea09e0b60
rusber364 <rusber364@gmail.com>
2022-10-15 17:28:31 +0300

    fix type, delete handler, set default lang en

--- app/screens/main/HomeScreen.tsx
+++ app/screens/main/HomeScreen.tsx
@@ -7,19 +7,15 @@ import { Paper } from '../../layout/Paper'
 export function HomeScreen() {
   const { i18n } = useTranslation()
 
-  const changeLanguage = (lang: 'ru' | 'en') => () => {
-    i18n.changeLanguage(lang)
-  }
-
   return (
     <Paper>
       <NavigationMainMenu />
       <View style={{ alignSelf: 'auto', flexDirection: 'row', margin: 5 }}>
         <View style={{ margin: 10 }}>
-          <Button onPress={changeLanguage('en')}>EN</Button>
+          <Button onPress={() => i18n.changeLanguage('en')}>EN</Button>
         </View>
         <View style={{ margin: 10 }}>
-          <Button onPress={changeLanguage('ru')}>RU</Button>
+          <Button onPress={() => i18n.changeLanguage('ru')}>RU</Button>
         </View>
       </View>
     </Paper>
