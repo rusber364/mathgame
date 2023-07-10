5dbd1d96d5385dc2d97bb4bb6425af6ed8af5329
rusber364 <rusber364@gmail.com>
2023-07-10 18:54:40 +0300

    add options loopReplaceLetter

--- src/features/game/utils/loopReplaceLetter.ts
+++ src/features/game/utils/loopReplaceLetter.ts
@@ -1,8 +1,15 @@
 import { getRandomNumber } from './getRandomNumber'
 
-export function loopReplaceLetter(operation: string, letter = 'N') {
+type Options = {
+  max?: number
+  min?: number
+  letter?: string
+}
+
+export function loopReplaceLetter(operation: string, options: Options = {}) {
+  const { letter = 'N', max, min } = options
   while (operation.includes(letter)) {
-    operation = operation.replace(letter, String(getRandomNumber()))
+    operation = operation.replace(letter, String(getRandomNumber(max, min)))
   }
 
   return operation
