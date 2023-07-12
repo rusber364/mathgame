3e9b996908f4ade6b5496f1a4bee26ab63d1a7b8
rusber364 <rusber364@gmail.com>
2023-07-12 11:38:26 +0300

    delete SafeAreaView for LanguagesScreen

--- app/languages.tsx
+++ app/languages.tsx
@@ -1,5 +1,5 @@
 import { useCallback } from 'react'
-import { View, StyleSheet, FlatList, SafeAreaView, StatusBar } from 'react-native'
+import { StyleSheet, FlatList } from 'react-native'
 import { useTranslation } from 'react-i18next'
 import { useRouter } from 'expo-router'
 
@@ -19,31 +19,16 @@ export default function LanguagesScreen() {
   )
 
   return (
-    <SafeAreaView style={styles.root}>
-      <FlatList
-        style={styles.listLang}
-        data={languageKeys}
-        renderItem={(lang) => (
-          <View style={styles.lang}>
-            <Button onPress={handleChangeLang(lang.item)}>{lang.item}</Button>
-          </View>
-        )}
-      />
-    </SafeAreaView>
+    <FlatList
+      style={styles.listLang}
+      data={languageKeys}
+      renderItem={(lang) => <Button onPress={handleChangeLang(lang.item)}>{lang.item}</Button>}
+    />
   )
 }
 
 const styles = StyleSheet.create({
-  root: {
-    flex: 1,
-    marginVertical: StatusBar.currentHeight || 0,
-  },
   listLang: {
-    paddingHorizontal: 10,
-  },
-  lang: {
-    flexGrow: 1,
-    flexBasis: '40%',
-    marginHorizontal: 10,
+    padding: 15,
   },
 })
