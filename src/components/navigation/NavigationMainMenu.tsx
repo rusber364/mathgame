da630432db649e489a59a84855f28b19f2b5d898
rusber364 <rusber364@gmail.com>
2023-07-12 11:51:30 +0300

    update navigation main menu with link comp

--- src/components/navigation/NavigationMainMenu.tsx
+++ src/components/navigation/NavigationMainMenu.tsx
@@ -1,28 +1,37 @@
 import { View } from 'react-native'
-import { useRouter } from 'expo-router'
+import { Link } from 'expo-router'
 import { useTranslation } from 'react-i18next'
 
 import { Button } from '~/components/common/Button'
 
 export function NavigationMainMenu() {
   const { t } = useTranslation()
-  const router = useRouter()
-
-  const navigateToTeam = () => router.push('team')
-  const navigateToGameMode = () => router.push('game-mode')
-  const navigateToAchievement = () => router.push('achievement')
-  const navigateToLeague = () => router.push('league')
-  const navigateToOption = () => router.push('option')
-  const navigateToPurchase = () => router.push('purchase')
 
   return (
     <View>
-      <Button onPress={navigateToGameMode}>{t('screens.main.game')}</Button>
-      <Button onPress={navigateToTeam}>{t('screens.main.team')}</Button>
-      <Button onPress={navigateToAchievement}>{t('screens.main.achievements')}</Button>
-      <Button onPress={navigateToLeague}>{t('screens.main.league')}</Button>
-      <Button onPress={navigateToOption}>Option</Button>
-      <Button onPress={navigateToPurchase}>Purchase</Button>
+      <Link asChild href="game-mode">
+        <Button>{t('screens.main.game')}</Button>
+      </Link>
+
+      <Link asChild href="team">
+        <Button>{t('screens.main.team')}</Button>
+      </Link>
+
+      <Link asChild href="achievement">
+        <Button>{t('screens.main.achievements')}</Button>
+      </Link>
+
+      <Link asChild href="league">
+        <Button>{t('screens.main.league')}</Button>
+      </Link>
+
+      <Link asChild href="option">
+        <Button>Option</Button>
+      </Link>
+
+      <Link asChild href="purchase">
+        <Button>Purchase</Button>
+      </Link>
     </View>
   )
 }
