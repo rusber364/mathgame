0d22c894dcb188a3e1bfbcac6cb7c44747528283
Okko <okko1994@gmail.com>
2023-08-03 13:37:33 +0300

    add NavigationBack

---
+++ src/components/common/NavigationBack.tsx
@@ -0,0 +1,33 @@
+import { useRouter } from 'expo-router'
+import { TouchableOpacity, Image, StyleSheet } from 'react-native'
+
+import arrow from '~/assets/arrow.png'
+
+export function NavigationBack() {
+  const router = useRouter()
+
+  if (!router.canGoBack()) return null
+
+  const navigationToBack = () => router.back()
+
+  return (
+    <TouchableOpacity style={styles.headerLeftButton} onPress={navigationToBack}>
+      <Image source={arrow} style={styles.headerLeftImage} />
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  headerLeftButton: {
+    borderRadius: 30,
+    paddingVertical: 1,
+    paddingHorizontal: 10,
+    marginHorizontal: 1,
+    backgroundColor: '#f33',
+  },
+  headerLeftImage: {
+    fontWeight: 'bold',
+    width: 54,
+    height: 54,
+  },
+})
