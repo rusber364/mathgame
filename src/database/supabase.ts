d059c15ce57ac10fe68dc32a921ccd29888db8f9
rusber364 <rusber364@gmail.com>
2023-08-23 01:40:52 +0300

    add types env

--- src/database/supabase.ts
+++ src/database/supabase.ts
@@ -3,8 +3,8 @@ import 'react-native-url-polyfill/auto'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL ?? ''
-const SUPABASE_KEY = process.env.EXPO_PUBLIC_SUPABASE_KEY ?? ''
+const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL
+const SUPABASE_KEY = process.env.EXPO_PUBLIC_SUPABASE_KEY
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
   auth: {
---
+++ src/types/env.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS {
+  declare interface ProcessEnv {
+    EXPO_PUBLIC_SUPABASE_URL: string
+    EXPO_PUBLIC_SUPABASE_KEY: string
+  }
+}
