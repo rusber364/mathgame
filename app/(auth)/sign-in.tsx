523dad0df03be42a51b8647cdee5bc7077beab8a
Okko <okko1994@gmail.com>
2023-08-22 21:38:36 +0300

    update auth style with react native paper

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,12 +1,13 @@
 import { useRouter } from 'expo-router'
 import { useState } from 'react'
 import { KeyboardAvoidingView, Platform, View } from 'react-native'
-import { TextInput } from 'react-native-paper'
+import { StyleSheet, Text } from 'react-native'
+import { Button, TextInput } from 'react-native-paper'
 import Toast from 'react-native-toast-message'
 
-import { Button } from '~/components/common/button.comp'
 import { supabase } from '~/database/supabase'
 import { AuthTitle } from '~/features/auth/components/auth-title'
+import { delay } from '~/utils/delay'
 
 export default function SignInScreen() {
   const [email, setEmail] = useState('')
@@ -19,9 +20,14 @@ export default function SignInScreen() {
   async function handleLogin() {
     setLoading(true)
     const { error } = await supabase.auth.signInWithPassword({ email, password })
+    // TODO: delete delay
+    await delay(1000)
 
     if (!error) {
-      router.push('/index/profile')
+      // TODO: fix types route profile
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      router.push('/profile')
     } else {
       Toast.show({
         type: 'error',
@@ -29,36 +35,55 @@ export default function SignInScreen() {
         text2: error.message,
       })
     }
-
     setLoading(false)
   }
-
   return (
     <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={{ flex: 1 }}>
-      <View style={{ justifyContent: 'center', flexGrow: 1 }}>
+      <View style={styles.root}>
         <AuthTitle>Login</AuthTitle>
-        <View style={{ marginVertical: 20 }}>
-          <TextInput
-            label="Email"
-            value={email}
-            onChangeText={setEmail}
-            placeholder="Enter your email"
-            keyboardType="email-address"
-          />
-          <TextInput
-            right={<TextInput.Icon icon="eye" onPress={() => setShowPassword(!isShowPassword)} />}
-            label="Password"
-            value={password}
-            onChangeText={setPassword}
-            placeholder="Enter your password"
-            secureTextEntry={isShowPassword}
-          />
-        </View>
-        <Button onPress={handleLogin} isLoading={isLoading}>
+        <TextInput
+          style={styles.textInput}
+          label="Email"
+          value={email}
+          onChangeText={setEmail}
+          placeholder="Enter your email"
+          keyboardType="email-address"
+        />
+        <TextInput
+          style={styles.textInput}
+          right={<TextInput.Icon icon="eye" onPress={() => setShowPassword(!isShowPassword)} />}
+          label="Password"
+          value={password}
+          onChangeText={setPassword}
+          placeholder="Enter your password"
+          secureTextEntry={isShowPassword}
+        />
+        <Button mode="contained" loading={isLoading} onPress={handleLogin}>
           Login
         </Button>
-        <Button onPress={() => router.push('/sign-up')}>Registration</Button>
+        <View style={styles.container}>
+          <Text>Not a member?</Text>
+          <Button onPress={() => router.push('/sign-up')}>Registration</Button>
+        </View>
       </View>
     </KeyboardAvoidingView>
   )
 }
+const styles = StyleSheet.create({
+  container: {
+    padding: 30,
+    alignItems: 'center',
+  },
+  textInput: {
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 5,
+  },
+  root: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    marginBottom: 10,
+    padding: 20,
+  },
+})
