0675a086672b462dc19ec289c59fabf237a35c23
rusber364 <rusber364@gmail.com>
2023-09-01 17:58:52 +0300

    add jest-expo

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,13 @@
 import { type JestConfigWithTsJest } from 'ts-jest'
 
 const config: JestConfigWithTsJest = {
-  preset: '@testing-library/react-native',
+  preset: 'jest-expo',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js', '<rootDir>/tests.setup.ts'],
-  transformIgnorePatterns: ['node_modules/(?!((jest-)?react-native(-.*)?|@react-native(-community)?)/)'],
-  moduleNameMapper: {
-    '\\.svg': '<rootDir>/__mocks__/svgMock.js',
-  },
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|nanoid)',
+  ],
 }
 
 export default config
