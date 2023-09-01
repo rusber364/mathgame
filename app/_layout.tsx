050db386be1a6fbd0a9df91486efd589e395ffc5
rusber364 <rusber364@gmail.com>
2023-09-01 18:09:36 +0300

    add I18nextProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,24 @@
-import '~/langs/translation'
-
 import { Stack } from 'expo-router'
+import { I18nextProvider } from 'react-i18next'
 import { StyleSheet } from 'react-native'
 import ToastMessage from 'react-native-toast-message'
 import { Provider } from 'react-redux'
 
 import { AuthProvider } from '~/features/auth/context/auth.context'
+import i18n from '~/langs/translation'
 import { store } from '~/store/redux'
 
 export default function Layout() {
   return (
     <>
       <Provider store={store}>
-        <AuthProvider>
-          <Stack screenOptions={{ contentStyle: styles.stack }}>
-            <Stack.Screen name="(main)/index" options={{ headerShown: false }} />
-          </Stack>
-        </AuthProvider>
+        <I18nextProvider i18n={i18n}>
+          <AuthProvider>
+            <Stack screenOptions={{ contentStyle: styles.stack }}>
+              <Stack.Screen name="(main)/index" options={{ headerShown: false }} />
+            </Stack>
+          </AuthProvider>
+        </I18nextProvider>
       </Provider>
       <ToastMessage />
     </>
