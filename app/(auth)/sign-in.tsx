c59a793d1b5da98e275d78cd495187055dce4f66
Okko <okko1994@gmail.com>
2023-09-06 14:36:05 +0300

    add auth buttons

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,11 @@
-import AuthButtonLogin from '~/features/auth/components/auth-button-login'
+import AuthButton from '~/features/auth/components/auth-button'
 import { AuthFields } from '~/features/auth/components/auth-fields'
 import AuthSubButton from '~/features/auth/components/auth-sub-button'
 
 export default function SingIndScreen() {
   return (
     <AuthFields title="login">
-      <AuthButtonLogin />
+      <AuthButton type="login">Login</AuthButton>
       <AuthSubButton label="Registration" textCaption="Not a memeber?" href="/sign-up"></AuthSubButton>
     </AuthFields>
   )
--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,11 +1,11 @@
-import AuthButtonRegistration from '~/features/auth/components/auth-button-registration'
+import AuthButton from '~/features/auth/components/auth-button'
 import { AuthFields } from '~/features/auth/components/auth-fields'
 import AuthSubButton from '~/features/auth/components/auth-sub-button'
 
 export default function SingIndScreen() {
   return (
     <AuthFields title="Registration">
-      <AuthButtonRegistration />
+      <AuthButton type="registration">Registration</AuthButton>
       <AuthSubButton label="Login" textCaption="Already have an account?" href="/sign-in"></AuthSubButton>
     </AuthFields>
   )
