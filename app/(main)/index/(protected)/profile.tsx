b39c440fe8ba5fd68618eee0f2258225fa333b4b
Illia-Adamchuk <illya.Adamchuk@gmail.com>
2023-09-24 20:31:21 -0400

    updateAvatarScreen

--- app/(main)/index/(protected)/profile.tsx
+++ app/(main)/index/(protected)/profile.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { StyleSheet, View } from 'react-native'
-import { Button, TextInput } from 'react-native-paper'
+import { Button, IconButton, TextInput } from 'react-native-paper'
 
 import { DrawerContainer } from '~/components/common/drawer-container'
 import { supabase } from '~/database/supabase'
@@ -46,23 +46,33 @@ export default function ProfileScreen() {
 
   return (
     <DrawerContainer>
-      <View style={styles.center}>
-        <Avatar size={200} />
+      <View style={styles.avatar}>
+        <Avatar size={300} />
       </View>
-      <View>
-        <TextInput
-          label="nickname"
-          placeholder="your nickname..."
-          value={nickname}
-          onChangeText={setNickname}
-          disabled={isUpdatingProfile}
-        />
-        <Button
-          icon={isUpdatingProfile ? 'account-edit' : 'account-edit-outline'}
-          onPress={() => setUpdatingProfile(!isUpdatingProfile)}
-        >
-          {isUpdatingProfile ? 'Change nickname' : 'Save nickname'}
-        </Button>
+      <View style={styles.container}>
+        <View style={styles.object1}>
+          <TextInput
+            label="nickname"
+            placeholder="your nickname..."
+            value={nickname}
+            onChangeText={setNickname}
+            disabled={isUpdatingProfile}
+          />
+        </View>
+        <View style={styles.object2}>
+          <IconButton
+            icon={isUpdatingProfile ? 'account-edit' : 'account-edit-outline'}
+            size={50}
+            iconColor="red"
+            onPress={() => setUpdatingProfile(!isUpdatingProfile)}
+          >
+            {' '}
+            {isUpdatingProfile ? isUpdatingProfile : !isUpdatingProfile}
+          </IconButton>
+          {/* <Button></Button> */}
+        </View>
+      </View>
+      <View style={styles.updateProfile}>
         <Button icon="update" onPress={handleUpdateProfile} disabled={!isUpdatingProfile}>
           Update profile
         </Button>
@@ -71,12 +81,35 @@ export default function ProfileScreen() {
   )
 }
 const styles = StyleSheet.create({
-  center: {
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  avatar: {
+    width: 'auto',
+    height: '50%',
+    margin: '5%',
+    padding: '1%',
     textAlign: 'center',
     alignItems: 'center',
-    width: '50%',
-    height: '50%',
-    marginLeft: '25%',
-    marginRight: '25%',
+  },
+  input: {},
+  updateProfile: {
+    height: '20%',
+    alignItems: 'center',
+  },
+  object1: {
+    width: '95%',
+    height: '15%',
+  },
+  object2: {
+    width: '5%',
+    height: '10%',
+    alignItems: 'center',
+    textAlign: 'center',
+    margin: -10,
+    padding: 0,
+    position: 'relative',
+    top: 'auto',
   },
 })
