15dbdb9579b02818b7eefc96cc12d96e70a47421
rusber364 <rusber364@gmail.com>
2024-01-04 01:07:27 +0200

    hook useFetchAvatar

--- src/features/profile/components/avatar.tsx
+++ src/features/profile/components/avatar.tsx
@@ -1,12 +1,7 @@
-import * as DocumentPicker from 'expo-document-picker'
-import { useEffect, useState } from 'react'
 import { Platform, StyleSheet, View } from 'react-native'
 import { Avatar as AvatarPaper } from 'react-native-paper'
-import Toast from 'react-native-toast-message'
 
-import { supabase } from '~/database/supabase'
-import { useAuthContext } from '~/features/auth/context/auth.context'
-import { blobToBase64 } from '~/utils/blobToBase64'
+import { useFetchAvatar } from '../hooks/use-fetch-avatar'
 
 interface Props {
   size: number
@@ -15,55 +10,7 @@ interface Props {
 }
 
 export default function Avatar({ size = 150 }: Props) {
-  const [avatarUrl, setAvatarUrl] = useState<string>()
-  const { session } = useAuthContext()
-  const userId = session?.user.id
-
-  useEffect(() => {
-    async function downloadImage() {
-      try {
-        const { data, error } = await supabase.storage.from('avatars').download(`${userId}.avatar`)
-        if (error) {
-          throw error
-        }
-
-        const avatar = await blobToBase64(data)
-        setAvatarUrl(avatar)
-      } catch (error) {
-        if (error instanceof Error) {
-          console.log('Error downloading image:', error.message)
-        }
-      }
-    }
-
-    downloadImage()
-  }, [userId])
-
-  async function uploadAvatar() {
-    try {
-      const images = await DocumentPicker.getDocumentAsync({ type: 'images/*' })
-      if (images.assets?.length) {
-        const avatar = images.assets[0]
-        const avatarFile = avatar.file
-        if (avatarFile) {
-          const storage = supabase.storage.from('avatars')
-          const { error } = await storage.upload(`${userId}.avatar`, avatarFile, { upsert: true })
-          if (error) {
-            throw error
-          }
-          setAvatarUrl(avatar.uri)
-        }
-      }
-    } catch (error) {
-      if (error instanceof Error) {
-        Toast.show({
-          type: 'error',
-          text1: error.name,
-          text2: error.message,
-        })
-      }
-    }
-  }
+  const { avatarUrl, uploadAvatar } = useFetchAvatar()
 
   return (
     <View
---
+++ src/features/profile/hooks/use-fetch-avatar.ts
@@ -0,0 +1,61 @@
+import * as DocumentPicker from 'expo-document-picker'
+import { useEffect, useState } from 'react'
+import Toast from 'react-native-toast-message'
+
+import { supabase } from '~/database/supabase'
+import { useAuthContext } from '~/features/auth/context/auth.context'
+import { blobToBase64 } from '~/utils/blobToBase64'
+
+export function useFetchAvatar() {
+  const [avatarUrl, setAvatarUrl] = useState<string>()
+  const { session } = useAuthContext()
+  const userId = session?.user.id
+
+  useEffect(() => {
+    async function downloadAvatar() {
+      try {
+        const { data, error } = await supabase.storage.from('avatars').download(`${userId}.avatar`)
+        if (error) {
+          throw error
+        }
+
+        const avatar = await blobToBase64(data)
+        setAvatarUrl(avatar)
+      } catch (error) {
+        if (error instanceof Error) {
+          console.log('Error downloading avatar:', error.message)
+        }
+      }
+    }
+
+    downloadAvatar()
+  }, [userId])
+
+  async function uploadAvatar() {
+    try {
+      const images = await DocumentPicker.getDocumentAsync({ type: 'images/*' })
+      if (images.assets?.length) {
+        const avatar = images.assets[0]
+        const avatarFile = avatar.file
+        if (avatarFile) {
+          const storage = supabase.storage.from('avatars')
+          const { error } = await storage.upload(`${userId}.avatar`, avatarFile, { upsert: true })
+          if (error) {
+            throw error
+          }
+          setAvatarUrl(avatar.uri)
+        }
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        Toast.show({
+          type: 'error',
+          text1: error.name,
+          text2: error.message,
+        })
+      }
+    }
+  }
+
+  return { avatarUrl, uploadAvatar }
+}
