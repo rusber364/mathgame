6cdb6d272a07af12975522ef3ed442ff066ca235
rusber364 <rusber364@gmail.com>
2024-01-08 00:02:53 +0200

    use fragment

--- app/(league)/(tournamentScreen)/tournamentScreen.tsx
+++ app/(league)/(tournamentScreen)/tournamentScreen.tsx
@@ -1,36 +1,40 @@
-import { useEffect, useState } from 'react'
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
 const arr = [
-    { digits: 1, name: '', reward: { attempt: 1 } },
-    { digits: 1, name: 'First Day', reward: { attempt: 2 } },
-    { digits: 2, name: 'Second Day', reward: { attempt: 3 } },
-    { digits: 3, name: '30 Right Answers', reward: { attempt: 4 } },
-    { digits: 4, name: 'Third days strict', reward: { attempt: 5 } },
-    { digits: 5, name: '50 Right Answers strict', reward: { attempt: 6 } },
+  { digits: 1, name: '', reward: { attempt: 1 } },
+  { digits: 1, name: 'First Day', reward: { attempt: 2 } },
+  { digits: 2, name: 'Second Day', reward: { attempt: 3 } },
+  { digits: 3, name: '30 Right Answers', reward: { attempt: 4 } },
+  { digits: 4, name: 'Third days strict', reward: { attempt: 5 } },
+  { digits: 5, name: '50 Right Answers strict', reward: { attempt: 6 } },
 ]
 export default function Tournament() {
-    const [rewards, setRewards] = useState(arr)
+  const [rewards, setRewards] = useState(arr)
 
-    return (
-        <View>
+  return (
+    <View>
+      <View>
+        <Text>Single Achievements</Text>
+      </View>
+      <View>
+        {rewards.map((reward) => (
+          <>
             <View>
-                <Text>Single Achievements</Text>
+              <Text>{reward.digits}</Text>
             </View>
             <View>
-                {rewards.map((reward) => (
-                    <View key={reward.digits}>
-                        <Text>{reward.digits}</Text>
-                    </View>
-                    <View key={reward.name}>
-                        <Text>{reward.name}</Text>
-                    </View>
-                    <View key={reward.reward}> 
-                    <Text>{reward.reward.attempt}</Text>
-                    </View>
-                ))}
+              <Text>{reward.name}</Text>
             </View>
-        </View>
-    )
+            <View>
+              <Text>{reward.reward.attempt}</Text>
+            </View>
+          </>
+        ))}
+      </View>
+    </View>
+  )
 }
+
 const styles = StyleSheet.create({})
